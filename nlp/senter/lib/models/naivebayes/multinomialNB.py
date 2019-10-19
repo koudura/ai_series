ea08f4e6da5f9f883948a362cccc63200c59decc
koudura <Jehovah8420>
2019-10-19 01:59:04 +0100

    complete multinomial NB

--- nlp/senter/lib/models/naivebayes/multinomialNB.py
+++ nlp/senter/lib/models/naivebayes/multinomialNB.py
@@ -1,6 +1,6 @@
 import math
 import operator
-from typing import List, Iterable, Tuple
+from typing import List, Tuple
 
 from nlp.senter.lib.models.naivebayes.NB import NaiveBayes
 from nlp.senter.lib.parsers.textparser import TextParser
@@ -11,16 +11,21 @@ class MultinomialNB(NaiveBayes):
     def __init__(self, dataset: Tuple[List, List], parser: TextParser, classes: Tuple = ('0', '1')):
 
         super().__init__(dataset, parser, classes)
+
         self.class_probabilities = {}
+        self.function = ''
 
     def train(self, function="log"):
         super(MultinomialNB, self).train(function)
 
         assert len(self.X_train) == len(self.Y_train)
         assert len(self.classes) > 1
+        assert self.function in {"log", "laplace"}
 
         label_size = len(self.Y_train)
         train_size = len(self.X_train)
+
+        self.function = function
 
         """
         Initialize class probabilities for each class.
@@ -53,86 +58,81 @@ class MultinomialNB(NaiveBayes):
             for _class in self.class_probabilities:
                 self.class_probabilities[_class] /= label_size
 
+    def predict(self, sample):
+        super(MultinomialNB, self).predict(sample)
+
+        return self.__log__(sample) if self.function == "log" else self.__laplace__(sample)
 
     def test(self):
-        pass
+        return [self.__log__(data) if self.function == "log" else self.__laplace__(data) for data in self.X_test]
 
-    def predict(self, sample):
-        pass
-
-    def fit(self, train_x: List, labels_y: List, classes: Iterable = ('0', '1')):
+    def __log__(self, sample: str) -> str:
         """
-        Fit training data and labels into naive bayes model. \n
-        :type labels_y: List \n
-        :type train_x: List \n
-        :param train_x: training data-set to be fitted into model.
-        :param labels_y: corresponding labels of training set to be fit into naive model.
-        :param classes:
-        :return: Fits object model with given data-set.
+        Use the logarithmic probability scaling
+        function to estimate probability of class predictors \n
+        :type sample: str
+        :param sample: use model to predict class of sample data.
+        :return: predicted class value.
         """
-        assert len(train_x) == len(labels_y)
-        assert len(list(classes)) > 1
+        """
+        initialize class predictor model : P(c|x)
+        """
+        predictor = {_class: self.class_probabilities[_class] for _class in self.class_probabilities}
 
         """
-        Initialize class probabilities for each class.
+        estimate class prior probabilities given a sample data.
         """
-        self.class_probabilities = {x: 0 for x in classes}
+        for word in sample:
+            for c in self.model:
+                if word in self.model[c]:
+                    predictor[c] += math.log(self.model[c][word])
         """
-        Create empty bag of words(BoW) model for each class in naive-model. 
+        :return maximum predicted class probability. i.e.
+        given classes = ['0', '1'], if P(0|sample) > P(1|sample) return '0', otherwise '1'.
         """
-        self.model = {x: {} for x in classes}
-        """
-        Iterate through training data-set for each training-sentence to create BoW model.
-        """
-        for i in range(len(train_x)):
-            """
-            For each label increment its corresponding count for class probability. 
-            """
-            self.class_probabilities[labels_y[i]] += 1
-            """
-            fill BoW model.
-            """
-            for word in train_x[i]:
-                if word in self.model[labels_y[i]]:
-                    self.model[labels_y[i]][word] += 1
-                else:
-                    self.model[labels_y[i]][word] = 1
-            """
-            Divide count of words per class by number labels, 
-            to give probabilities of classes.
-            """
-            for _class in self.class_probabilities:
-                self.class_probabilities[_class] /= len(labels_y)
+        return max(predictor.items(), key=operator.itemgetter(1))[0]
 
-    def classify(self, sentence):
+    def __laplace__(self, sample):
         """
+        Using the laplacian function for bag of words and probability smoothening
+        to estimate probability of class predictors \n
+        :param sample:  use model to predict class of sample data.
+        :return: predicted class value.
+        """
+        aprior_model = {x: {} for x in self.class_probabilities}
+        predictor = {_class: self.class_probabilities[_class] for _class in self.class_probabilities}
 
-        :param sentence:
-        :return:
-        """
-
-        """
-
-        """
-        predict_model = {_class: {} for _class in self.class_probabilities}
-        """
-
-        """
-        prediction = {_class: 1 for _class in self.class_probabilities}
-
-        """
-
-        """
-        for word in sentence:
+        for word in sample:
             for _class in self.model:
                 if word in self.model[_class]:
-                    predict_model[_class][word] = self.model[_class][word] + 1
+                    aprior_model[_class][word] = self.model[_class][word] + 1
                 else:
-                    predict_model[_class][word] = 1
+                    aprior_model[_class][word] = 1
 
         """
         For each class(c) in classes in naive model evaluate prediction
         via naive-bayes model-> || P(c|X) = (P(X|c) * P(c) / P(X) || 
         """
-        for _class in predict_model:
-            for word in predict_model[_class]: ...
+        for c in aprior_model:
+            for word in aprior_model[c]:
+                if len(aprior_model) > 0:
+                    aprior_model[c][word] /= (sum(self.model[c].values()) + len(self.model[c]) + 1)
+                    """
+                    || P(x|c) || -> :A
+                    """
+                    predictor[c] *= aprior_model[c][word]
+            """
+            || P(x|c) * P(c) || :A * P(c) -> :B
+            """
+            predictor[c] *= self.class_probabilities[c]
+            """
+            || P(x|c) * P(c) / P(x) || :B / P(x) -> :C
+            """
+            if len(aprior_model[c] > 0):
+                predictor[c] /= sum(aprior_model[c].values())
+
+        """
+        :return maximum predicted class probability. i.e.
+        given classes = ['0', '1'], if P(0|sample) > P(1|sample) return '0', otherwise '1'.
+        """
+        return max(predictor.items(), key=operator.itemgetter(1))[0]
